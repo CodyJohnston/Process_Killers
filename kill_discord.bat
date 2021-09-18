74eb861b72ed959ce3b5a41ebb0596ca3a2deea7
CodyJohnston <teamrocketops@gmail.com>
2021-09-18 13:57:41 -0700

    Add files via upload

---
+++ kill_discord.bat
@@ -0,0 +1,17 @@
+@echo off
+
+REM this file firce kills Discord when it is being a bitch
+REM It does not rely an any depenedencies and can be run fron any Windows instalation
+REM Written by @AdwareHunter
+
+:check_admin
+	net session  >nul 2>&1
+if %errorlevel% == 0 (
+	taskkill /f /im "discord.exe"
+) else (
+	echo "You must run this task as administrator."
+	pause >nul 
+)
+
+
+
---
+++ kill_edge.bat
@@ -0,0 +1,14 @@
+@echo off
+
+REM this file firce kills Edge when it is being a bitch
+REM It does not rely an any depenedencies and can be run fron any Windows instalation
+REM Written by @AdwareHunter
+
+:check_admin
+	net session  >nul 2>&1
+if %errorlevel% == 0 (
+	taskkill /f /im "msedge.exe"
+) else (
+	echo "You must run this task as administrator."
+	pause >nul 
+)
